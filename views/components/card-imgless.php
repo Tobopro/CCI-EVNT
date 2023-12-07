79131435e44e6a17927b447cc18a40ec83626f64
Nicolas Kirchhoffer <nkirchhoffer@gmail.com>
2023-12-07 12:24:20 +0100

    Tree structure and carousel card homepage

--- views/components/card-imgless.php
+++ views/components/card-imgless.php
@@ -1,37 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./SASS/main.css">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" rel="stylesheet"
-        crossorigin="anonymous">
-    <title>Document</title>
-</head>
-
-<body>
-    <div class="card-il-desktop">
-        <div class="card-il-desktop__bot">
-            <div class="card-il-desktop__bot-left">
-                <div class="card-il-desktop__bot-left-content">
-                    <h2>Titre de l'evnt</h2>
-                    <h3>Nom de l'organisateur</h3>
-                </div>
-                <a class="card-il-desktop__bot-left-btn" href="">Page de l'Evnt</a>
+<div class="card-il-desktop">
+    <div class="card-il-desktop__bot">
+        <div class="card-il-desktop__bot-left">
+            <div class="card-il-desktop__bot-left-content">
+                <h2>Titre de l'evnt</h2>
+                <h3>Nom de l'organisateur</h3>
             </div>
-            <div class="card-il-desktop__bot-right">
-                <div class="card-il-desktop__bot-right-i">
-                    <i class="fa-regular fa-heart card-il-desktop__bot-right-i-like"></i>
-                    <i class="fa-solid fa-share-nodes card-il-desktop__bot-right-i-share"></i>
-                </div>
-                <div class="card-il-desktop__bot-right-u">
-                    <i class="fa-solid fa-user"></i>
-                    <p>3/12</p>
-                </div>
+            <a class="card-il-desktop__bot-left-btn" href="">Page de l'Evnt</a>
+        </div>
+        <div class="card-il-desktop__bot-right">
+            <div class="card-il-desktop__bot-right-i">
+                <i class="fa-regular fa-heart card-il-desktop__bot-right-i-like"></i>
+                <i class="fa-solid fa-share-nodes card-il-desktop__bot-right-i-share"></i>
+            </div>
+            <div class="card-il-desktop__bot-right-u">
+                <i class="fa-solid fa-user"></i>
+                <p>3/12</p>
             </div>
         </div>
     </div>
-</body>
-
-</html>
+</div>
