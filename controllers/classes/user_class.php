35269f14e7dba6d71bf801c18932d1dab95d7591
leoKowalski <leo.kowalski4872@gmail.com>
2023-12-11 10:20:22 +0100

    added user class attributes

--- controllers/classes/user_class.php
+++ controllers/classes/user_class.php
@@ -2,3 +2,42 @@
 
 
 <?php
+
+class User {
+    // attributes
+    private $id;
+
+    private $email;
+    private $password;
+    private $firstName;
+    private $lastName;
+    private $city;
+    private $description;
+    private $profilePicture;
+    private $coverPicture;
+
+    private $isBanned;
+
+    private $participationsCount;
+    private $creationsCount;
+    private $prefferedCategories;
+
+    private $evntsToCome;
+    private $evntsParticipated;
+    private $evntsCreated;
+    private $evntsLiked;
+
+    private $friends;
+    private $friendRequests;
+    private $friendRequestsSent;
+    private $blockedUsers;
+    private $blockedBy;
+
+    // methods 
+
+    // constructor
+    
+
+
+
+}
