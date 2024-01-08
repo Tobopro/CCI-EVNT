3772104a903ffc7f4a5f1fe4262113d1b67b6bc0
Nicolas Kirchhoffer <nkirchhoffer@gmail.com>
2024-01-08 14:58:24 +0100

    create user

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -8,11 +8,12 @@ class UsersController
 
     public function index()
     {
-        require_once base_path('Views/profile_creation_page.php');
+
     }
     public function create()
     {
         $actionURL = "?url=creation_profil";
+        require_once base_path('Views/profile_creation_page.php');
 
     }
 }
