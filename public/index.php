9dbfe9c0df0cb2a95befa9dd4d62a19bf2865f32
Nicolas Kirchhoffer <nkirchhoffer@gmail.com>
2024-01-08 13:03:09 +0100

    form profile creation

--- public/index.php
+++ public/index.php
@@ -19,7 +19,7 @@
 
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-    </script>
+        </script>
     <script defer src="../leaflet/leaflet.js"></script>
     <script defer src="./assets/javascript/dashboard.js"></script>
     <script defer type="text/javascript" src="assets/javascript/script-homepage.js"></script>
@@ -71,6 +71,9 @@ if (isset($_GET['url'])) {
             break;
         case 'mentions':
             require '../views/legal.php';
+            break;
+        case 'creation_profil':
+            require '../views/profile_creation_page.php';
             break;
         default:
             require '../views/homepage.php';
---
+++ views/profile_creation_page.php
@@ -0,0 +1,21 @@
+<main>
+    <h2>Création de compte</h2>
+    <form action="?url=creation_profil" method="POST">
+        <label for="lastName">Nom:</label>
+        <input type="text" name="lastName" required>
+
+        <label for="firstName">Prénom:</label>
+        <input type="text" name="firstName" required>
+
+        <label for="mail">Email:</label>
+        <input type="email" name="mail" required>
+
+        <label for="password">Mot de passe:</label>
+        <input type="password" name="password" required>
+
+        <label for="password">Confirmer le mot de passe:</label>
+        <input type="password" name="password" required>
+
+        <button type="submit">Valider</button>
+    </form>
+</main>
