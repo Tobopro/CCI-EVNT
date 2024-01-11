431ab19fd81052497779194fc610997751039d0f
Tom Boyer <tompro.boyer@gmail.com>
2024-01-11 15:01:18 +0100

    my_events view/controller

---
+++ views/my_events.php
@@ -0,0 +1,31 @@
+<div class="container-fluid">
+    <div class="row d-flex justify-content-center">
+        <h2 class="title-section">Tous les événements</h2>
+    
+        <?php foreach ($hydratedEvents as $hydratedEvent) : ?>
+       
+            <article class="evnt-single col-3 m-5">
+                <a href="">
+                    <img class="img-une" src="assets/img/energetic-dancer-dynamic-glamorous-light.jpg"
+                        alt="photographie d'un groupe de fêtard" />
+                    <div class="padding-evnt">
+                        <ul class="details-evnt">
+                            <!-- Ville -->
+                            <li><img src="assets/img/icons/icon-map-marker.svg" alt="icone map marker" /> <?= $hydratedEvent->getAdress() ?></li>
+                            <!-- Date -->
+                            <li><img src="assets/img/icons/icon-calendar.svg" alt="icone calendrier" /> <?= $hydratedEvent->getDateEvnt() ?></li>
+                            <!-- Participants -->
+                            <li><img src="assets/img/icons/icon-group.svg" alt="icone groupe" /> <?= $hydratedEvent->getNbParticipants() ?>/<?= $hydratedEvent->getNbParticipants() ?></li>
+                        </ul>
+                        <h3><?= $hydratedEvent->getTitle() ?></h3>
+                        <ul class="liste-categories">
+                            <!-- Add your category logic here -->
+                        </ul>
+                            <a class="btn btn-danger" href="handlers/evnt_delete_handler.php?id=<?= $hydratedEvent->getId() ?>">Delete</a>
+
+                    </div>
+                </a>
+            </article>
+        <?php endforeach; ?>
+    </div>
+</div>
